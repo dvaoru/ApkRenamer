ddd6ec569589323345f1300610eae09b9facac5c
dvaoru <dvaoru@gmail.com>
2019-10-16 00:05:03 +0300

    Share to GitHub

--- src/ru/gavrikov/Main.java
+++ src/ru/gavrikov/Main.java
@@ -54,7 +54,7 @@ public class Main {
                 "\n" +
                 "Use the renamer program to change an app name, a package name and an icon in an Android app.\n" +
                 "\n" +
-                "Usage: java -jar renamer.jar [-a path/to/app.apk] [-o path/to/renamed_app.apk] [-n new_name] [-p new.package.name] [-i new_icon.png]] \n" +
+                "Usage: java -jar renamer.jar [-a path/to/app.apk] [-o path/to/renamed_app.apk] [-n new_name] [-p new.package.name] [-i new_icon.png] \n" +
                 "\n" +
                 "You can place app.apk to \"in\" folder, new_icon.png to \"icon\" folder \n" +
                 "and run java -jar renamer.jar without arguments. Your renamed_app.apk will be placed in \"out\" folder";
